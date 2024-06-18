fdc8bcfd615c73f3bc97a7793546ffc5d385c76f
mydevitsandeep <165661615+mydevitsandeep@users.noreply.github.com>
2024-06-18 11:35:57 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 14455
+let accountEmail = "sdsa@google.com"
+var accountPassword = "12345"
+
+accountCity = "jaipur"
+
+let accountState;
+
+// accountId = "aaaaa" // const never be change to any value
+
+accountEmail= "sads@dscdsds.com"
+accountPassword= "334343"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodeJS not browser and alert not work like this in NodeJS
+
+let age = 18;
+let loggedIn = "sandeep";
+let nameFirst = true;
+
+console.log(typeof age);
+console.log(typeof loggedIn);
+console.log(typeof nameFirst);
+
